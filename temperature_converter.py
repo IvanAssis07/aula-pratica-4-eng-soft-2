b61f592f4877f2364e4a68ffca0045682988e11f
IvanAssis07 <you@example.com>
2024-12-09 17:45:26 -0300

    feature: created temperature converter and its tests.

---
+++ temperature_converter.py
@@ -0,0 +1,21 @@
+def celsius_to_fahrenheit(celsius):
+    return celsius * 1.8 + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+def celsius_to_kelvin(celsius):
+    return celsius + 273.15
+
+def kelvin_to_celsius(kelvin):
+    if kelvin < 0:
+        raise ValueError("Temperature in Kelvin cannot be negative.")
+    return kelvin - 273.15
+
+def fahrenheit_to_kelvin(fahrenheit):
+    celsius = fahrenheit_to_celsius(fahrenheit)
+    return celsius_to_kelvin(celsius)
+
+def kelvin_to_fahrenheit(kelvin):
+    celsius = kelvin_to_celsius(kelvin)
+    return celsius_to_fahrenheit(celsius)
